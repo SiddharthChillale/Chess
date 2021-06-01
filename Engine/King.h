5b152af523fd10b8db0c9e093b5c6c98eb13f3cd
RomanSpecter <roman.astakhov2020@gmail.com>
2021-06-01 16:23:08 +0300

    Reworked class King

--- Engine/King.h
+++ Engine/King.h
@@ -6,6 +6,6 @@ class King :public Piece
 {
 public:
 
-	King( class Cell& cell, bool in_isLightSide );
-	void Move( class Board& brd, const Location& move_vec, Cell& nxt_pos ) override;
+	King( class CellArray::Cell& cell, bool in_isLightSide );
+	void Move( class Board& brd ) override;
 };
