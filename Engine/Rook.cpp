fabe2be1fb7334b7506825b76201fb85ae6c93d1
RomanSpecter <roman.astakhov2020@gmail.com>
2021-06-01 16:38:26 +0300

    Reworked class Rook

--- Engine/Rook.cpp
+++ Engine/Rook.cpp
@@ -1,38 +1,39 @@
 #include "Rook.h"
 #include "Board.h"
 
-Rook::Rook( Cell& cell, bool in_isLightSide )
+Rook::Rook( class CellArray::Cell& cell, bool in_isLightSide )
 	:
 	Piece( cell, in_isLightSide )
 {
 }
 
-void Rook::Move( Board& brd, const Location& move_vec, Cell& nxt_pos )
+void Rook::Move( class Board& brd )
 {
 	const bool currentTurn = brd.GetCurrentTurn();
+	CellArray::Cell& next_cell = brd.GetNextCell();
+	const Location& cur_pos = brd.GetIdx( brd.GetCurrentCell().location );
+	const Location& nxt_pos = brd.GetIdx( brd.GetNextCell().location );
+	const Location& move_vec = Board::GetMoveVec( cur_pos, nxt_pos );
 
-	const Location cur_loc = brd.PixelToCellDim( cell->GetLocation() );
-	Cell& cur_pos = brd.GetCell( cur_loc.x, cur_loc.y );
-
-	if( IsHorizontal( move_vec ) || IsVertical(move_vec) )
+	if( brd.IsHorizontal( move_vec ) || brd.IsVertical(move_vec) )
 	{
-		if( PathIsFree( brd, move_vec, nxt_pos ) )
+		if( brd.PathIsFree( move_vec ) )
 		{
-			if( nxt_pos.IsFree() || IsEnemyCell( nxt_pos ) )
+			if( next_cell.IsFree() || brd.NextIsEnemy() )
 			{
-				cell->MovePieceTo( nxt_pos );
-				cell = &nxt_pos;
+				cell->MovePieceTo( next_cell );
+				cell = &next_cell;
 				brd.SetNextSideMove();
 			}
 		}
 	}
 
-	brd.DeselectTargetCell();
+	brd.DeselectLastCell();
 
 	const bool nextTurn = currentTurn != brd.GetCurrentTurn();
 	if( nextTurn )
 	{
 		isMoved = true;
-		brd.DeselectBothCells();
+		brd.DeselectAllCells();
 	}
 }
--- Engine/Rook.h
+++ Engine/Rook.h
@@ -6,6 +6,6 @@ class Rook : public Piece
 {
 
 public:
-	Rook( class Cell& cell, bool in_isLightSide );
-	void Move( class Board& brd, const Location& move_vec, Cell& nxt_pos ) override;
+	Rook( class CellArray::Cell& cell, bool in_isLightSide );
+	void Move( class Board& brd ) override;
 };
